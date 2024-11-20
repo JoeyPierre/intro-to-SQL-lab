b62fd5b71cc79ba63cfd07d9e227241778bd48b2
Joey Pierre <JoeyPierre@joeypierre27@gmail.com>
2024-11-20 10:35:27 -0500

    done

--- clues.sql
+++ clues.sql
@@ -2,25 +2,57 @@
  
 -- Write SQL query here
 
+SELECT * FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
+
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
 
+SELECT * FROM countrylanguages WHERE countrycode = 'VAT';
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
+
+SELECT * FROM countrylanguages
+JOIN countries
+ON countrycode = code
+WHERE language = 'Italian' AND region = 'Southern Europe'
+ORDER BY percentage;
+
+countrycode | language | isofficial | percentage | code |             name              | continent |     region      | surfacearea | indepyear | population | lifeexpectancy |    gnp     |   gnpold   |           localname           |      governmentform      |     headofstate      | capital | code2
+-- -------------+----------+------------+------------+------+-------------------------------+-----------+-----------------+-------------+-----------+------------+----------------+------------+------------+-------------------------------+--------------------------+----------------------+---------+-------
+--  VAT         | Italian  | t          |          0 | VAT  | Holy See (Vatican City State) | Europe    | Southern Europe |         0.4 |      1929 |       1000 |                |       9.00 |            | Santa Sede/Città del Vaticano | Independent Church State | Johannes Paavali II  |    3538 | VA
+--  ITA         | Italian  | t          |       94.1 | ITA  | Italy                         | Europe    | Southern Europe |      301316 |      1861 |   57680000 |             79 | 1161755.00 | 1145372.00 | Italia                        | Republic                 | Carlo Azeglio Ciampi |    1464 | IT
+--  SMR         | Italian  | t          |        100 | SMR  | San Marino                    | Europe    | Southern Europe |          61 |       885 |      27000 |           81.1 |     510.00 |            | San Marino                    | Republic                 |                      |    3171 | SM
+-- (3 rows)
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
 
+SELECT * FROM cities WHERE countrycode = 'SMR' AND NOT name = 'San Marino';
+
+--   id  |    name    | countrycode |     district      | population
+-- ------+------------+-------------+-------------------+------------
+--  3170 | Serravalle | SMR         | Serravalle/Dogano |     
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
+
+SELECT * FROM cities
+JOIN countries
+ON countrycode = code
+WHERE cities.name LIKE 'Serra%' AND NOT cities.name = 'Serravalle';
+
+--  id  | name  | countrycode |    district    | population | code |  name  |   continent   |    region     | surfacearea  | indepyear | population | lifeexpectancy |    gnp    |  gnpold   | localname |  governmentform  |        headofstate        | capital | code2
+-- -----+-------+-------------+----------------+------------+------+--------+---------------+---------------+--------------+-----------+------------+----------------+-----------+-----------+-----------+------------------+---------------------------+---------+-------
+--  265 | Serra | BRA         | Espírito Santo |     302666 | BRA  | Brazil | South America | South America | 8.547403e+06 |      1822 |  170115000 |           62.9 | 776739.00 | 804108.00 | Brasil    | Federal Republic | Fernando Henrique Cardoso |     211 | BR
+-- (1 row)
+
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -28,6 +60,17 @@
 -- follow right behind you!
 
 -- Write SQL query here
+
+SELECT capital FROM COUNTRIES
+WHERE code = 'BRA';
+SELECT * FROM cities
+JOIN countries
+ON capital = cities.id
+WHERE code = 'BRA'
+
+-- id  |   name   | countrycode |     district     | population | code |  name  |   continent   |    region     | surfacearea  | indepyear | population | lifeexpectancy |    gnp    |  gnpold   | localname |  governmentform  |        headofstate        | capital | code2
+-- -----+----------+-------------+------------------+------------+------+--------+---------------+---------------+--------------+-----------+------------+----------------+-----------+-----------+-----------+------------------+---------------------------+---------+-------
+--  211 | Brasília | BRA         | Distrito Federal |    1969868 | BRA  | Brazil | South America | South America | 8.547403e+06 |      1822 |  170115000 |           62.9 | 776739.00 | 804108.00 | Brasil    | Federal Republic | Fernando Henrique Cardoso |     211 | BR
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -39,6 +82,12 @@
 --               I need a little more sunshine with my slice of life.
 --               So I'm off to add one to the population I find
 --               In a city of ninety-one thousand and now, eighty five.
+
+SELECT * FROM cities
+WHERE population = 91084
+-- id  |     name     | countrycode |  district  | population
+-- ------+--------------+-------------+------------+------------
+--  4060 | Santa Monica | USA         | California |      91084
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
